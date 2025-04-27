30c4c5366704b282638ac950a95327020b66c67f
flechtenberg <fabian.lechtenberg@chem.ethz.ch>
2025-04-27 18:38:20 +0200

    Add setup.py for package configuration and dependencies

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pulpo-gui",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "pulpo",
+        "streamlit",
+    ],
+    entry_points={
+        'console_scripts': [
+            'pulpo-gui = pulpo_gui.app:main'
+        ]
+    }
+)
